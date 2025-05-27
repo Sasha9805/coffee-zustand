bd36e41ddd8b954fbc168ce96b4630a9d9ab0c58
Sasha Pimenov <pimenovs33@gmail.com>
2025-05-27 21:27:20 +0300

    Added two actions

--- src/model/counterStore.ts
+++ src/model/counterStore.ts
@@ -4,8 +4,27 @@ type CounterState = {
   counter: number;
 };
 
-const counterSlice: StateCreator<CounterState> = () => ({
+type CounterActions = {
+  increment: () => void;
+  decrement: () => void;
+};
+
+const counterSlice: StateCreator<CounterState & CounterActions> = (
+  set,
+  get
+) => ({
   counter: 0,
+  decrement: () => {
+    const { counter } = get();
+    // set((state) => ({ ...state, counter: counter - 1 }));
+    set({ counter: counter - 1 });
+  },
+  increment: () => {
+    const { counter } = get();
+    set({ counter: counter + 1 });
+  },
 });
 
-export const useCounterStore = create<CounterState>(counterSlice);
+export const useCounterStore = create<CounterState & CounterActions>(
+  counterSlice
+);
